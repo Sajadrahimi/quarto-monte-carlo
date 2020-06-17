d9d5dd616f766f9edef6f897c07907bc09ba7491
Sajad <sajadrahimi@aut.ac.ir>
2020-06-18 00:07:27 +0430

    minor simplification!

--- manager.py
+++ manager.py
@@ -4,8 +4,6 @@ from game.Totem import *
 
 if __name__ == '__main__':
 	board = Board()
-	totems = []
-	for i in range(16):
-		totems.append(Totem("{0:04b}".format(i)))
+	totems = [Totem("{0:04b}".format(i)) for i in range(16)]
 
 
